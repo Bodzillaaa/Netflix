41c86436f2d09fe0bcc85841d4c2b06c285a194f
Bodzillaa <bodruddoza.araf@g.bracu.ac.bd>
2025-02-11 13:58:48 +0600

    For Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import mediaRoutes from "./routes/media.route.js";
@@ -12,6 +13,7 @@ import { protectRoute } from "./middlewares/protectRoute.js";
 const app = express();
 
 const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json());
@@ -19,6 +21,14 @@ app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/get", protectRoute, mediaRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
+
+if (ENV_VARS.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
