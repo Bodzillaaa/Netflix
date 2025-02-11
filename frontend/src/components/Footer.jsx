4743723b48a7dba2a3b0a8e3d2e8d83c421470b1
Bodzillaa <bodruddoza.araf@g.bracu.ac.bd>
2025-02-11 12:57:30 +0600

    Added my links in the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,22 +1,22 @@
 const Footer = () => {
   return (
-    <footer className="border-t border-gray-800 bg-black py-6 text-white md:px-8 md:py-8">
+    <footer className="border-t border-gray-800 bg-black py-6 text-gray-500 md:px-8 md:py-8">
       <div className="flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-muted-foreground text-center text-sm leading-loose text-balance md:text-left">
           Built by{" "}
           <a
-            href="#"
+            href="https://www.linkedin.com/in/bodruddoza-araf-5989a22b7/"
             target="_blank"
-            className="font-medium underline underline-offset-4"
+            className="font-medium text-gray-400 underline underline-offset-4 hover:text-gray-300"
           >
-            Bodruddoza Araf.{" "}
+            Bodruddoza Araf
           </a>
-          The source code is here...{" "}
+          . For the source code {">"}{" "}
           <a
-            href="#"
+            href="https://github.com/Bodzillaa"
             target="_blank"
             rel="noreferrer"
-            className="font-medium underline underline-offset-4"
+            className="font-medium text-gray-400 underline underline-offset-4 hover:text-gray-300"
           >
             Github
           </a>
